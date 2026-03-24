43f49d8bb4b2e918a9a451bdd78769453541223e
hellsing2030 <sebastiancastellanosfr@gmail.com>
2026-03-23 22:26:11 -0500

    add corner

--- src/assets/overlay/MainOverlay.tsx
+++ src/assets/overlay/MainOverlay.tsx
@@ -6,7 +6,14 @@ export const MainOverlay = () => {
         <span className="container-overlay-left"/>
         <div className="container-view-video-top-bottom">
           <span className="container-overlay-top"/>
-          <div className="cam-overlay"></div>
+          <div className="cam-overlay">
+            <div className="container-corner">
+              <span className="corner corner-1" />
+              <span className="corner corner-2" />
+              <span className="corner corner-3" />
+              <span className="corner corner-4" />
+            </div>
+          </div>
           <span className="container-overlay-bottom"/>
         </div>
         <span className="container-overlay-right"/>
